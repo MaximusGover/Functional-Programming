6b80c20e84a2c5112f727db7c31dbba048bfdb63
MaximusGover <maxidrue@gmail.com>
2024-02-21 01:55:46 +0000

    added week 3

--- Week3.hs
+++ Week3.hs
@@ -1,7 +1,8 @@
 -- We don't import '||' from the prelude, so that we can
 -- define our own version
 
-import Prelude hiding ((||), (&&))
+import System.Win32 (xBUTTON1)
+import Prelude hiding (gcd, (&&), (||))
 
 -- The following line declares the || operator (which we are about to
 -- re-define) to be right associative and to have precedence 2. This
@@ -9,6 +10,7 @@ import Prelude hiding ((||), (&&))
 -- valid (e.g. it sets the precedence of || to be lower than >).
 
 infixr 2 ||
+
 infixr 3 &&
 
 -- A naive re-implementation of the Prelude operator ||
@@ -19,14 +21,14 @@ True || False = True
 False || False = False
 
 -- An alternative re-implementation
---(||) :: Bool -> Bool -> Bool
---False || False   = False
---_ || _           = True
+-- (||) :: Bool -> Bool -> Bool
+-- False || False   = False
+-- _ || _           = True
 
 -- Another alternative re-implementation
---(||) :: Bool -> Bool -> Bool
---True || _     =  True
---False || a    = a
+-- (||) :: Bool -> Bool -> Bool
+-- True || _     =  True
+-- False || a    = a
 
 fact :: Int -> Int
 fact n
@@ -38,7 +40,7 @@ mult :: Int -> Int -> Int
 mult n m
   | n == 0 = 0
   | n > 0 = m + mult (n - 1) m
-  | otherwise = - mult (- n) m
+  | otherwise = -mult (-n) m
 
 divide :: Int -> Int -> Int
 divide n m
@@ -50,7 +52,9 @@ nor False x = not x
 nor True _ = False
 
 fibonacci :: Int -> Int
+
 -- fibonacci n
+
 -- | n == 0 = 0
 -- | n == 1 = 1
 -- | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
@@ -66,15 +70,80 @@ True && False = False
 False && False = False
 
 -- An alternative re-implementation
---(&&) :: Bool -> Bool -> Bool
+-- (&&) :: Bool -> Bool -> Bool
 -- && True   = True
---_ && _           = False
+-- _ && _           = False
 
 -- Another alternative re-implementation
---(&&) :: Bool -> Bool -> Bool
---False && _     =  False
---True && a    = a
+-- (&&) :: Bool -> Bool -> Bool
+-- False && _     =  False
+-- True && a    = a
 
 exOr :: Bool -> Bool -> Bool
 exOr False x = x
 exOr True x = not x
+
+ifThenElse :: Bool -> Int -> Int -> Int
+ifThenElse True x y = x
+ifThenElse _ x y = y
+
+daysInMonth :: Int -> Int
+daysInMonth 2 = 28
+daysInMonth n | odd n = 31
+daysInMonth n = 30
+
+validDate :: Int -> Int -> Bool
+validDate d m | daysInMonth m >= d = True
+validDate d m = False
+
+sumNumbers :: Int -> Int
+sumNumbers 0 = 0
+sumNumbers n = sumNumbers (n - 1) + n
+
+-- sumNumbers :: Int -> Int
+-- sumNumbers x
+--   | x == 0 = 0
+--   | otherwise = sumNumbers (x - 1) + x
+
+sumSquares :: Int -> Int
+sumSquares 0 = 0
+sumSquares n = sumSquares (n - 1) + n ^ 2
+
+-- sumSquares :: Int -> Int
+-- sumSquares n
+--   | n == 0 = 0
+--   | otherwise = sumSquares (n - 1) + n ^ 2
+
+power :: Int -> Int -> Int
+power v1 1 = v1
+power v1 v2 = power v1 (v2 - 1) * v1
+
+-- power :: Int -> Int -> Int
+-- power v1 v2
+--   | v2 == 1 = v1
+--   | otherwise = power v1 (v2 - 1) * v1
+
+sumFromTo :: Int -> Int -> Int
+sumFromTo v1 v2 | v1 > v2 = 0
+sumFromTo v1 v2 = sumFromTo (v1 + 1) v2 + v1
+
+-- sumFromTo :: Int -> Int -> Int
+-- sumFromTo v1 v2
+--   | v1 > v2 = 0
+--   | otherwise = sumFromTo (v1 + 1) v2 + v1
+
+gcd :: Int -> Int -> Int
+gcd v1 0 = v1
+gcd v1 v2 = gcd v2 (v1 `mod` v2)
+
+-- gcd :: Int -> Int -> Int
+-- gcd v1 v2
+--   | v2 == 0 = v1
+--   | otherwise = gcd v2 (v1 `mod` v2)
+
+intSquareRoot :: Int -> Int
+intSquareRoot n = findRoot n n
+
+findRoot :: Int -> Int -> Int
+findRoot n s | s * s <= n = s
+findRoot n s = findRoot n (s - 1)
